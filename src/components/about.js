2306f4cb8a140025cf502dec975c5fdd6bd49794
TedtheDev <tylerjedmunds@gmail.com>
2018-02-16 15:46:42 -0600

    updated and added links

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const About = (props) => {
   return (
     <div>
       About route
+      <Link to='/'>Home path</Link>
     </div>
   )
 }
